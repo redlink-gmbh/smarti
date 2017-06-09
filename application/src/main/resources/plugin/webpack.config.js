e2f68d1b5e3a2c2165023783b89e9166e978527f
Thomas Kurz <thomas.kurz@salzburgresearch.at>
2017-06-09 15:05:01 +0200

    half functional plugin

--- application/src/main/resources/plugin/webpack.config.js
+++ application/src/main/resources/plugin/webpack.config.js
@@ -1,9 +1,21 @@
-var path = require('path');
+const path = require('path');
 
 module.exports = {
-    entry: './main.js',
+    entry: './',
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [{
+            test: /\.scss$/,
+            use: [{
+                loader: "style-loader" // creates style nodes from JS strings
+            }, {
+                loader: "css-loader" // translates CSS into CommonJS
+            }, {
+                loader: "sass-loader" // compiles Sass to CSS
+            }]
+        }]
     }
 };
