5ecd5a10e8e211487c62833e886d03da78c9ee71
Jakob Frank <jakob.frank@redlink.co>
2017-06-29 16:57:05 +0200

    Use correct serializer for ObjectId

--- application/src/main/java/io/redlink/smarti/webservice/pojo/QueryUpdate.java
+++ application/src/main/java/io/redlink/smarti/webservice/pojo/QueryUpdate.java
@@ -5,20 +5,23 @@ package io.redlink.smarti.webservice.pojo;
 
 import io.redlink.smarti.model.Slot;
 import io.redlink.smarti.model.Token;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 
 import java.util.Collection;
 import java.util.LinkedList;
 import java.util.List;
 
 /**
- * Created by jakob on 29.06.17.
  */
+@ApiModel
 public class QueryUpdate {
 
+    @ApiModelProperty("full list of tokens")
     private List<Token> tokens = new LinkedList<>();
 
+    @ApiModelProperty("updated slot-assignments of the update")
     private Collection<Slot> slots = new LinkedList<>();
-
 
     public List<Token> getTokens() {
         return tokens;
--- application/src/main/java/io/redlink/smarti/webservice/pojo/SmartiUpdatePing.java
+++ application/src/main/java/io/redlink/smarti/webservice/pojo/SmartiUpdatePing.java
@@ -3,16 +3,28 @@
  */
 package io.redlink.smarti.webservice.pojo;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import org.bson.types.ObjectId;
 
 /**
  */
+@ApiModel
 public class SmartiUpdatePing {
 
+    @ApiModelProperty
+    @JsonSerialize(using = ToStringSerializer.class)
     private final ObjectId conversationId;
+
+    @ApiModelProperty
     private final String token;
 
-    public SmartiUpdatePing(ObjectId conversationId, String token) {
+    @JsonCreator
+    public SmartiUpdatePing(@JsonProperty ObjectId conversationId, @JsonProperty String token) {
         this.conversationId = conversationId;
         this.token = token;
     }
