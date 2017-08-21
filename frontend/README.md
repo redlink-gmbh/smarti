516153e46d0564caf39b98cf03f27b16e8f8c09a
Jakob Frank <jakob.frank@redlink.co>
2017-08-21 08:46:44 +0200

    Included frontend into maven build and packaging workflow (#29)

--- frontend/README.md
+++ frontend/README.md
@@ -1,8 +1,3 @@
-# smarti
-
-This project is generated with [yo angular generator](https://github.com/yeoman/generator-angular)
-version 0.14.0.
-
 ## Build & development
 
 Run `grunt` for building and `grunt serve` for preview.
--- frontend/src/Gruntfile.js
+++ frontend/src/Gruntfile.js
@@ -23,7 +23,7 @@ module.exports = function (grunt) {
   // Configurable paths for the application
   var appConfig = {
     app: require('./bower.json').appPath || 'app',
-    dist: 'dist'
+    dist: '../target/classes/public'
   };
 
   // Define the configuration for all the tasks
