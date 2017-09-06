93ef3a8ee2be0d11f5b9b7d8399187f5fb4cf2c7
Rupert Westenthaler <rupert.westenthaler@gmail.com>
2017-09-06 10:25:29 +0200

    minor: replaces `\t` with spaces and added `”numOfRows”: 10”`

--- dist/src/main/resources/migration/0.3-to-0.5.js
+++ dist/src/main/resources/migration/0.3-to-0.5.js
@@ -42,78 +42,79 @@ db.getCollection(client).find().forEach(function(c) {
             created: new ISODate(),
             modified: new ISODate(),
             config: {
-        		"queryBuilder" : [
-        			{
-        				"name" : "conversationmlt",
-        				"displayName" : "Related Conversations",
-        				"type" : "conversationmlt",
-        				"enabled" : true,
-        				"unbound" : false,
-        				"configuration" : {
-        					
-        				}
-        			},
-        			{
-        				"solrEndpoint" : "http://host.domain.org:8983/solr/change-me",
-        				"search" : {
-        					"title" : {
-        						"enabled" : false,
-        						"field" : ""
-        					},
-        					"fullText" : {
-        						"enabled" : true,
-        						"field" : ""
-        					},
-        					"spatial" : {
-        						"enabled" : true,
-        						"locationNameField" : ""
-        					},
-        					"temporal" : {
-        						"enabled" : false
-        					},
-        					"related" : {
-        						"enabled" : false,
-        						"fields" : [ ]
-        					}
-        				},
-        				"defaults" : {
-        					"rows" : 10,
-        					"fields" : "*,score"
-        				},
-        				"result" : {
-        					"mappings" : {
-        						"title": "title",
-        				        "description": "description",
-        				        "type": "type",
-        				        "doctype": "doctype",
-        				        "thumb": "thumb",
-        				        "link": "link",
-        				        "date": "date",
-        				        "source": "source"
-        					},
-        				},
-        				"name" : "db-search-endpoint",
-        				"displayName" : "DB Search Endpoint",
-        				"type" : "solrsearch",
-        				"enabled" : false,
-        				"unbound" : false,
-        				"configuration" : {
-        					
-        				},
-        				"_class" : "io.redlink.smarti.query.solr.SolrEndpointConfiguration"
-        			},
-        			{
-        				"name" : "conversationsearch",
-        				"displayName" : "conversationsearch",
-        				"type" : "conversationsearch",
-        				"enabled" : true,
-        				"unbound" : false,
-        				"configuration" : {
+                "queryBuilder" : [
+                    {
+                        "name" : "conversationmlt",
+                        "displayName" : "Related Conversations",
+                        "type" : "conversationmlt",
+                        "enabled" : true,
+                        "unbound" : false,
+                        "configuration" : {
+                            
+                        }
+                    },
+                    {
+                        "solrEndpoint" : "http://host.domain.org:8983/solr/change-me",
+                        "search" : {
+                            "title" : {
+                                "enabled" : false,
+                                "field" : ""
+                            },
+                            "fullText" : {
+                                "enabled" : true,
+                                "field" : ""
+                            },
+                            "spatial" : {
+                                "enabled" : true,
+                                "locationNameField" : ""
+                            },
+                            "temporal" : {
+                                "enabled" : false
+                            },
+                            "related" : {
+                                "enabled" : false,
+                                "fields" : [ ]
+                            }
+                        },
+                        "defaults" : {
+                            "rows" : 10,
+                            "fields" : "*,score"
+                        },
+                        "result" : {
+                            "numOfRows": 10,
+                            "mappings" : {
+                                "title": "title",
+                                "description": "description",
+                                "type": "type",
+                                "doctype": "doctype",
+                                "thumb": "thumb",
+                                "link": "link",
+                                "date": "date",
+                                "source": "source"
+                            },
+                        },
+                        "name" : "search-endpoint-0",
+                        "displayName" : "Search Endpoint",
+                        "type" : "solrsearch",
+                        "enabled" : false,
+                        "unbound" : false,
+                        "configuration" : {
+                            
+                        },
+                        "_class" : "io.redlink.smarti.query.solr.SolrEndpointConfiguration"
+                    },
+                    {
+                        "name" : "conversationsearch",
+                        "displayName" : "conversationsearch",
+                        "type" : "conversationsearch",
+                        "enabled" : true,
+                        "unbound" : false,
+                        "configuration" : {
 
-        				}
-        			}
-        		]
-        	}
+                        }
+                    }
+                ]
+            }
         }},
         { upsert: true }
     );
