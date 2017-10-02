410d3676542585ee027a3b3e440e7c1ffd6d9c91
Jakob Frank <jakob.frank@redlink.co>
2017-10-02 11:54:08 +0200

    lock down admin-ui (#94)

--- application/src/main/java/io/redlink/smarti/auth/simple/SimpleAuthConfiguration.java
+++ application/src/main/java/io/redlink/smarti/auth/simple/SimpleAuthConfiguration.java
@@ -42,10 +42,8 @@ public class SimpleAuthConfiguration extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
             .authorizeRequests()
-                .antMatchers("/admin/**").hasRole("ADMIN")
-                .and()
-            .authorizeRequests()
-                .anyRequest().permitAll()
+                .antMatchers("/rocket/**", "/conversation/**").permitAll()
+                .anyRequest().hasRole("ADMIN")
                 .and()
             .httpBasic()
                 .realmName("smarti admin area")
@@ -61,7 +59,7 @@ public class SimpleAuthConfiguration extends WebSecurityConfigurerAdapter {
             password = UUID.randomUUID().toString();
             log.error("No password configured in 'security.password', using '{}' for user 'admin'", password);
         } else {
-            log.info("Configuring passwort from 'security.password' for user 'admin'");
+            log.info("Configuring password from 'security.password' for user 'admin'");
         }
         auth
             .inMemoryAuthentication()
