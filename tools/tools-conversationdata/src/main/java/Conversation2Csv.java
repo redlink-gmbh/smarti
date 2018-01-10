62530fb4d8490b0f82a2e6cd552ac55502f77875
Rupert Westenthaler <rupert.westenthaler@gmail.com>
2018-01-10 12:08:18 +0100

    wip

---
+++ tools/tools-conversationdata/src/main/java/Conversation2Csv.java
@@ -0,0 +1,13 @@
+
+public class Conversation2Csv {
+
+    
+    private static final org.apache.commons.cli.Option options = new Options();
+    
+    
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
