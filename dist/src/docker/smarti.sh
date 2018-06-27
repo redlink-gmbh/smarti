a820607423ed23db4aa4af1b4a619620c74beb01
Oliver Jägle <github@beimir.net>
2018-06-27 11:55:00 +0200

    Hack: Add environment variable for doanload of NLP libs

--- dist/src/docker/smarti.sh
+++ dist/src/docker/smarti.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
-
+# TODO: remove this hack, don't use a dedicated proxy for the download
 if [ ! -f /opt/ext/stanford-corenlp-3.8.0.jar ]; then
-    curl -o /opt/ext/stanford-corenlp-3.8.0.jar https://repo1.maven.org/maven2/edu/stanford/nlp/stanford-corenlp/3.8.0/stanford-corenlp-3.8.0.jar
+    HTTPS_PROXY=$DOWNLOAD_PROXY curl -o /opt/ext/stanford-corenlp-3.8.0.jar https://repo1.maven.org/maven2/edu/stanford/nlp/stanford-corenlp/3.8.0/stanford-corenlp-3.8.0.jar
 fi
 
 if [ ! -f /opt/ext/stanford-corenlp-3.8.0-models-german.jar ]; then
-    curl -o /opt/ext/stanford-corenlp-3.8.0-models-german.jar https://repo1.maven.org/maven2/edu/stanford/nlp/stanford-corenlp/3.8.0/stanford-corenlp-3.8.0-models-german.jar
+    HTTPS_PROXY=$DOWNLOAD_PROXY curl -o /opt/ext/stanford-corenlp-3.8.0-models-german.jar https://repo1.maven.org/maven2/edu/stanford/nlp/stanford-corenlp/3.8.0/stanford-corenlp-3.8.0-models-german.jar
 fi
 
 if [ $# -eq 0 -o "${1:0:1}" = '-' ]; then
