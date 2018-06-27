1b0931477e94e101ad6e913e1f0de9b94f1b33d2
ThomasRoehl <thomas.t.roehl@deutschebahn.com>
2018-06-27 10:33:29 +0200

    download nlp libs on docker container start

--- dist/src/docker/smarti.sh
+++ dist/src/docker/smarti.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 
+if [ ! -f /opt/ext/stanford-corenlp-3.8.0.jar ]; then
+    curl -o /opt/ext/ https://repo1.maven.org/maven2/edu/stanford/nlp/stanford-corenlp/3.8.0/stanford-corenlp-3.8.0.jar
+fi
+
+if [ ! -f /opt/ext/stanford-corenlp-3.8.0-models-german.jar ]; then
+    curl -o /opt/ext/ https://repo1.maven.org/maven2/edu/stanford/nlp/stanford-corenlp/3.8.0/stanford-corenlp-3.8.0-models-german.jar
+fi
+
 if [ $# -eq 0 -o "${1:0:1}" = '-' ]; then
     JVM_ARGS="-XX:+UnlockExperimentalVMOptions -XX:+UseCGroupMemoryLimitForHeap -XX:MaxRAMFraction=1 -Djava.security.egd=file:/dev/./urandom -Dloader.path=BOOT-INF/lib,/opt/ext"
 
