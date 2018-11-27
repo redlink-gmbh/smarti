41144467dbc8db1bddbb1bdddfd500f5a052031d
Peyman Aparviz <peyman.aparviz@redlink.co>
2018-11-27 12:05:06 +0100

    #297: Fix reference error on refresh

--- integration/rocket-chat/main.js
+++ integration/rocket-chat/main.js
@@ -274,9 +274,9 @@ function Smarti(options) {
         });
     }
 
-    function refresh() {
+    function refresh(onFailure) {
         if(conversationId) {
-            getConversation(conversationId, showError);
+            getConversation(conversationId, onFailure);
         } else {
             Console.warn("Widget data refresh failed. Conversation ID not found!");
         }
@@ -1890,7 +1890,7 @@ function SmartiWidget(element, _options) {
         } else {
             $.observable(widgetHeaderTagsTemplateData.exclude).insert(tokenData.value.trim().toLowerCase());
             $.observable(widgetHeaderTagsTemplateData.tokens).remove(tokenIdx);
-            smarti.refresh();
+            smarti.refresh(showError);
         }
 
         tracker.trackEvent('tag.remove', tokenIdx);
@@ -1931,14 +1931,14 @@ function SmartiWidget(element, _options) {
             $.observable(widgetHeaderTagsTemplateData.exclude).insert(t.value.trim().toLowerCase());
         });
         $.observable(widgetHeaderTagsTemplateData.tokens).refresh([]);
-        smarti.refresh();
+        smarti.refresh(showError);
         tracker.trackEvent('tag.remove-all');
     });
 
     tags.on('click', '.reset-exclude', function() {
         $.observable(widgetHeaderTagsTemplateData.exclude).refresh([]);
         tracker.trackEvent('tag.reset-exclude');
-        smarti.refresh();
+        smarti.refresh(showError);
     });
 
     tags.on('click', 'li.add', function() {
